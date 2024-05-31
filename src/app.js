90e4a58b6c89f02b4261dcab2cf26cb08f63928c
akhilesshyantica <akhilesshyanticaw@gmail.com>
2024-05-31 17:36:35 +0700

    membuat bagian price pada produk menggunakan intl number format.

--- src/app.js
+++ src/app.js
@@ -9,3 +9,14 @@ document.addEventListener('alpine:init' , () => {
         ],
     }));
 });
+
+
+// konversi ke Rupiah
+
+const rupiah = (number) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0,
+    }).format(number);
+};
